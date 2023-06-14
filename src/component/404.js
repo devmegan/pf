3ab0a2ae7ffdbc96fa9fa751d4903915ca89dc18
devmegan <102955751+devmegan@users.noreply.github.com>
2023-06-14 13:49:15 +0000

    add: 404 component

---
+++ src/component/404.js
@@ -0,0 +1,8 @@
+export default function NotFound404() {
+    return (
+        <>
+            <h2>There was a problem finding that page. 🤯</h2>
+            <p>If you think something is missing, <a href="mailto:devmegan@protonmail.com" className="groovy-link">send me an email</a> and let me know.</p>
+        </>
+    )
+}
