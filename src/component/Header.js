f6eba10f575aa240277085ca9edc7c8d112608b1
devmegan <102955751+devmegan@users.noreply.github.com>
2023-06-14 20:35:30 +0000

    update: lighten header gradient

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,7 +2,7 @@ import { LogoDocker, LogoGithub, LogoLinkedin, Mail } from 'react-ionicons'
 
 export default function Header() {
     return (
-        <div className="container mx-auto max-w-screen-md bg-gradient-to-b from-sky-950 to-teal-900 rounded-md h-60 md:mt-5 lg:mt-10 grid place-items-center">
+        <div className="container mx-auto max-w-screen-md bg-gradient-to-b from-sky-950 to-teal-800 rounded-md h-60 md:mt-5 lg:mt-10 grid place-items-center">
             <h1 className="text-sky-900 font-black uppercase text-center text-6xl bg-stone-200 rounded-full py-4 px-5 w-22 h-22">
                 <span className="bg-clip-text text-transparent bg-gradient-to-b from-sky-900 to-teal-900">
                     M
