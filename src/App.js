55b4a60c105c4dfe0f46bebe947e0dd4871ff59d
devmegan <102955751+devmegan@users.noreply.github.com>
2023-06-14 14:40:26 +0000

    add: main container padding

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header from "./component/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App p-2">
       <Header />
       <div className="container mx-auto max-w-screen-sm text-lg min-h-screen">
         <HashRouter basename="/">
