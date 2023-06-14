4c2274aee63300a6c81dfe105592d9483dba3e6c
devmegan <102955751+devmegan@users.noreply.github.com>
2023-06-14 22:00:04 +0000

    add: init react ga

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-5L799JLCGD');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
