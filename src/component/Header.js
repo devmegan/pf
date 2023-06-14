b34402437ef0ef5c234bb1be6605bf311ec0daec
devmegan <102955751+devmegan@users.noreply.github.com>
2023-06-14 16:12:16 +0000

    update: reduce header margins on sm screens

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,13 +2,13 @@ import { LogoDocker, LogoGithub, LogoLinkedin, Mail } from 'react-ionicons'
 
 export default function Header() {
     return (
-        <div className="container mx-auto max-w-screen-md bg-gradient-to-b from-sky-950 to-teal-900 rounded-md h-60 sm:mt-10 grid place-items-center">
+        <div className="container mx-auto max-w-screen-md bg-gradient-to-b from-sky-950 to-teal-900 rounded-md h-60 md:mt-5 lg:mt-10 grid place-items-center">
             <h1 className="text-sky-900 font-black uppercase text-center text-6xl bg-stone-200 rounded-full py-4 px-5 w-22 h-22">
                 <span className="bg-clip-text text-transparent bg-gradient-to-b from-sky-900 to-teal-900">
                     M
                 </span>
             </h1>
-            <div className="flex gap-4" role="presentation">
+            <div className="flex gap-4">
                 <a href="https://www.linkedin.com/in/devmegan/" target="_blank" rel="noopener noreferrer">
                     <LogoLinkedin
                         color={'#fff'}
