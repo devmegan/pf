22aff5c6ce1673a58d532390709093a1319d9f79
devmegan <102955751+devmegan@users.noreply.github.com>
2023-06-14 13:48:28 +0000

    add: app header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { Routes, Route, HashRouter } from "react-router-dom";
 import Layout from './Layout';
 import About from "./component/About";
-import { LogoDocker, LogoGithub, LogoLinkedin, Mail } from 'react-ionicons'
 import NotFound404 from "./component/404";
 import Weeks from "./component/Weeks";
 import Feed from "./component/Feed";
 import Projects from "./component/Projects";
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <div className="container mx-auto max-w-screen-sm text-lg min-h-screen">
         <HashRouter basename="/">
           <Routes>
---
+++ src/component/Header.js
@@ -0,0 +1,47 @@
+import { LogoDocker, LogoGithub, LogoLinkedin, Mail } from 'react-ionicons'
+
+export default function Header() {
+    return (
+        <div className="container mx-auto max-w-screen-md bg-gradient-to-b from-sky-950 to-teal-900 rounded-md h-60 mt-10 grid place-items-center">
+            <h1 className="text-sky-900 font-black uppercase text-center text-6xl bg-stone-200 rounded-full py-4 px-5 w-22 h-22">
+                <span className="bg-clip-text text-transparent bg-gradient-to-b from-sky-900 to-teal-900">
+                    M
+                </span>
+            </h1>
+            <div className="flex gap-4" role="presentation">
+                <a href="https://www.linkedin.com/in/devmegan/" target="_blank" rel="noopener noreferrer">
+                    <LogoLinkedin
+                        color={'#fff'}
+                        height="30px"
+                        width="auto"
+                        cssClasses={"social-link"}
+                    />
+                </a>
+                <a href="https://github.com/devmegan">
+                    <LogoGithub
+                        color={'#fff'}
+                        height="30px"
+                        width="auto"
+                        cssClasses={"social-link"}
+                    />
+                </a>
+                <a href="https://hub.docker.com/u/devmegan">
+                    <LogoDocker
+                        color={'#fff'}
+                        height="34px"
+                        width="auto"
+                        cssClasses={"social-link"}
+                    />
+                </a>
+                <a href="mailto:devmegan@protonmail.com" className="my-auto">
+                    <Mail
+                        color={'#fff'}
+                        height="32px"
+                        width="auto"
+                        cssClasses={"social-link"}
+                    />
+                </a>
+            </div>
+        </div>
+    );
+}
