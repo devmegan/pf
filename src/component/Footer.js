436be66492fb36373094e833eda2bed05c8ac2a7
devmegan <102955751+devmegan@users.noreply.github.com>
2023-06-14 20:46:00 +0000

    add: aria labels to social links

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -4,7 +4,12 @@ export default function Footer() {
     return (
         <footer className="mt-12 mb-6">
             <div className="flex gap-4 justify-center">
-                <a href="https://www.linkedin.com/in/devmegan/" target="_blank" rel="noopener noreferrer">
+                <a
+                    aria-label="Go to my LinkedIn profile"
+                    href="https://www.linkedin.com/in/devmegan/" 
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
                     <LogoLinkedin
                         color={'#115e59'}
                         height="33px"
@@ -12,7 +17,12 @@ export default function Footer() {
                         cssClasses={"footer-link"}
                     />
                 </a>
-                <a href="https://github.com/devmegan"target="_blank" rel="noopener noreferrer">
+                <a
+                    aria-label="Check out my my GitHub account"
+                    href="https://github.com/devmegan" 
+                    rel="noopener noreferrer"
+                    target="_blank" 
+                >
                     <LogoGithub
                         color={'#115e59'}
                         height="33px"
@@ -20,7 +30,12 @@ export default function Footer() {
                         cssClasses={"footer-link"}
                     />
                 </a>
-                <a href="https://hub.docker.com/u/devmegan" target="_blank" rel="noopener noreferrer">
+                <a
+                    aria-label="Find me on Docker Hub" 
+                    href="https://hub.docker.com/u/devmegan" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
                     <LogoDocker
                         color={'#115e59'}
                         height="36px"
@@ -28,7 +43,11 @@ export default function Footer() {
                         cssClasses={"footer-link"}
                     />
                 </a>
-                <a href="mailto:devmegan@protonmail.com" className="my-auto">
+                <a
+                    aria-label="Send me an email"
+                    className="my-auto"
+                    href="mailto:devmegan@protonmail.com"
+                >
                     <Mail
                         color={'#115e59'}
                         height="34px"
--- src/component/Header.js
+++ src/component/Header.js
@@ -2,14 +2,19 @@ import { LogoDocker, LogoGithub, LogoLinkedin, Mail } from 'react-ionicons'
 
 export default function Header() {
     return (
-        <div className="container mx-auto max-w-screen-md bg-gradient-to-b from-sky-950 to-teal-800 rounded-md h-60 md:mt-5 lg:mt-10 grid place-items-center">
+        <header className="container mx-auto max-w-screen-md bg-gradient-to-b from-sky-950 to-teal-800 rounded-md h-60 md:mt-5 lg:mt-10 grid place-items-center">
             <h1 className="text-sky-900 font-black uppercase text-center text-6xl bg-stone-200 rounded-full py-4 px-5 w-22 h-22">
                 <span className="bg-clip-text text-transparent bg-gradient-to-b from-sky-900 to-teal-900">
                     M
                 </span>
             </h1>
             <div className="flex gap-4">
-                <a href="https://www.linkedin.com/in/devmegan/" target="_blank" rel="noopener noreferrer">
+                <a
+                    aria-label="Go to my LinkedIn profile"
+                    href="https://www.linkedin.com/in/devmegan/" 
+                    rel="noopener noreferrer"
+                    target="_blank"
+                >
                     <LogoLinkedin
                         color={'#fff'}
                         height="30px"
@@ -17,7 +22,12 @@ export default function Header() {
                         cssClasses={"social-link"}
                     />
                 </a>
-                <a href="https://github.com/devmegan" target="_blank" rel="noopener noreferrer">
+                <a 
+                    aria-label="Check out my my GitHub account"
+                    href="https://github.com/devmegan"
+                    rel="noopener noreferrer"
+                    target="_blank" 
+                >
                     <LogoGithub
                         color={'#fff'}
                         height="30px"
@@ -25,7 +35,12 @@ export default function Header() {
                         cssClasses={"social-link"}
                     />
                 </a>
-                <a href="https://hub.docker.com/u/devmegan" target="_blank" rel="noopener noreferrer">
+                <a
+                    aria-label="Find me on Docker Hub" 
+                    href="https://hub.docker.com/u/devmegan" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                >
                     <LogoDocker
                         color={'#fff'}
                         height="34px"
@@ -33,7 +48,11 @@ export default function Header() {
                         cssClasses={"social-link"}
                     />
                 </a>
-                <a href="mailto:devmegan@protonmail.com" className="my-auto">
+                <a 
+                    aria-label="Send me an email"
+                    className="my-auto"
+                    href="mailto:devmegan@protonmail.com" 
+                >
                     <Mail
                         color={'#fff'}
                         height="32px"
@@ -42,6 +61,6 @@ export default function Header() {
                     />
                 </a>
             </div>
-        </div>
+        </header>
     );
 }
